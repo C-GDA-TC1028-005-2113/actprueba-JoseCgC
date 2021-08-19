d0d5de483cc670cd5cc5c2ecd8a20479c0b39418
JoseCgC <A01642341@tec.mx>
2021-08-19 15:33:58 +0000

    Envio 1

--- assignments/01OperacionesBasicas/src/exercise.py
+++ assignments/01OperacionesBasicas/src/exercise.py
@@ -1,6 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-
+    a=int(input('Dame un número: '))
+    b=int(input('Dame un número: '))
+    suma= a + b
+    resta= a - b
+    multi= a * b
+    print(f'Suma: {suma}')
+    print(f'Resta: {resta}')
+    print(f'Multiplicación: {multi}')
 
 
 if __name__ == '__main__':
